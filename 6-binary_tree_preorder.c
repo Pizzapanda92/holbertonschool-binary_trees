335e29fac47abcefd0b9df9f22df23d49ad9838c
Your Name <you@example.com>
2024-08-05 15:00:22 +0000

    exo 6

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{ 
+
+	if (tree == NULL && func == NULL)
+		return;
+
+	func(tree->n);
+
+	if (tree->left)
+	{
+		binary_tree_preorder(tree->left, func);
+	}
+
+	if (tree->right)
+	{
+		binary_tree_preorder(tree->right, func);
+	}
+
+}
