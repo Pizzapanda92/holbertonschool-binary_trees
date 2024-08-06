fd7cc62db00ce9d5a79b5267722a51fc35e47df5
bluelightwatcher <9550@holbertonstudents.com>
2024-08-06 14:16:46 +0200

    Adding a root check

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -38,7 +38,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	if(tree->parent == NULL && tree->left == NULL && tree->right == NULL)
+	if(tree->left == NULL && tree->right == NULL)
 	{
 		return (1);
 	}
