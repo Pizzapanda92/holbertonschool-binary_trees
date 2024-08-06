ecc990e1f918b28896a47fc67a155dfe0dec9767
bluelightwatcher <9550@holbertonstudents.com>
2024-08-06 17:06:32 +0200

    Program computes balance factor of a tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,23 +1,33 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_balance - Computes the balance factor of a binary tree
+ * binary_tree_height - Computes the height of a binary tree
  * @tree: A pointer to the root node of the tree to traverse
- * Return: height of a b-tree or zero if tree is NULL
+ * Return: Height of the binary tree or 0 if tree is NULL
  */
-
-int binary_tree_balance(const binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
-	int left = 0, right = 0;
-
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left)
-		left = 1 + binary_tree_balance(tree->left);
+	size_t left_height = binary_tree_height(tree->left);
+	size_t right_height = binary_tree_height(tree->right);
 
-	if (tree->right)
-		right = 1 + binary_tree_balance(tree->right);
+	return ((left_height > right_height ? left_height : right_height) + 1);
+}
 
-	return (left - right);
+/**
+ * binary_tree_balance - Computes the balance factor of a binary tree
+ * @tree: A pointer to the root node of the tree to traverse
+ * Return: Balance factor of the binary tree or 0 if tree is NULL
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	int left_height = (tree->left) ? (int)binary_tree_height(tree->left) : 0;
+	int right_height = (tree->right) ? (int)binary_tree_height(tree->right) : 0;
+
+	return (left_height - right_height);
 }
