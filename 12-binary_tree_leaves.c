0d31b00b04e359d6cde531b6b45a3cf2f9518c85
bluelightwatcher <9550@holbertonstudents.com>
2024-08-06 14:07:43 +0200

    Adjusting is leaf conditions

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -19,11 +19,17 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 	}
 	if (node->right != NULL)
 	{
-		return(1);
+		return (1);
 	}
 
 	return (0);
 }
+
+/**
+ * binary_tree_leaves - counts number of leaves in a b-tree
+ * @tree: is a pointer to a node
+ * Return: number of leaves in a tree
+ */
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
@@ -35,12 +41,12 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 
 	if (tree->left)
 	{
-	left_leaves = binary_tree_is_leaf(tree->left);
-	}	
+		left_leaves = binary_tree_is_leaf(tree->left);
+	}
 
 	if (tree->right)
 	{
-	right_leaves = binary_tree_is_leaf(tree->right);
+		right_leaves = binary_tree_is_leaf(tree->right);
 	}
-	return(left_leaves + right_leaves);
+	return (left_leaves + right_leaves);
 }
