8fbf93696df9c453f64da1fe28bab8b6aed79f77
bluelightwatcher <9550@holbertonstudents.com>
2024-08-09 12:15:38 +0200

    Function checks for node sibblings

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - check for node sibblings
+ * @node: is the arbitrary node to check
+ * Return: pointer to sibbling node or NULL if none
+ */
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node->parent || !node->parent->left || !node->parent->right || !node)
+	{
+		return (NULL);
+	}
+
+	if (node == node->parent->left)
+	{
+		return (node->parent->right);
+	}
+	else
+	{
+		return (node->parent->left);
+	}
+}
